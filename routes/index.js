9f62a3f8b081169809d603269b190a56ae9c6385
DmitryShevtsov <orakooldsh@gmail.com>
2017-07-24 17:28:14 +0300

    Small refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+var dashboard = require('./dashboard');
+var users = require('./users');
+module.exports = [
+    dashboard,
+    users
+];
